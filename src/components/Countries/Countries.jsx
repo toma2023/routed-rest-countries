e946878896294d79aed8f96f7db57bee8bbabab7
toma2023 <mrinmoyehimadrita@gmail.com>
2023-04-03 15:18:54 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+
+const Countries = () => {
+    const countries = useLoaderData();
+    return (
+        <div>
+            <h3>All Countries: {countries.length}</h3>
+            <ul>
+                {
+                    countries.map(country=><li>{country.name.common} <Link to={`/country/${country.cca3}`}>Country Details</Link> </li> )
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Countries from './components/Countries/Countries';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Countries></Countries>,
+        loader: () => fetch('https://restcountries.com/v3.1/all')
+      },
+      {
+        path: 'country/:countryId',
+        element: <div></div>,
+        loader: ({params})=> fetch(`/${params.countryId}`)
+
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
